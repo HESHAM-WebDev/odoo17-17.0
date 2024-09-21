5293ef832c02c785037f2048f19c173ae84b05b0
HESHAM-WebDev <heshamwebdev@gmail.com>
2024-09-21 10:54:45 +0300

    add check num1

--- check_num/__manifest__.py
+++ check_num/__manifest__.py
@@ -15,7 +15,7 @@ Long description of module's purpose
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account','iet_analytic_employee_update'],
+    'depends': ['base','account'],
 
     # always loaded
     'data': [
